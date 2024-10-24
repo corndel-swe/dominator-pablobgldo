fd3e551520fade24e930695f0f9effac3fa457e9
pablobgldo <pablobgldo@gmail.com>
2024-10-24 16:29:28 +0100

    finished workshop

--- src/counter/script.js
+++ src/counter/script.js
@@ -1,1 +1,18 @@
 // TODO: increment and decrement the count when the buttons are clicked
+
+const decrement = document.getElementById('decrement');
+const increment = document.getElementById('increment');
+const count = document.getElementById('count');
+
+increment.addEventListener('click', () => {
+    count.textContent = parseInt(count.value) + 1;
+})
+
+decrement.addEventListener('click', () => {
+    count.textContent = parseInt(count.value) -1;
+})
+
+
+
+
+
--- src/demo/script.js
+++ src/demo/script.js
@@ -1,7 +1,22 @@
 // TODO: get a reference to the .box element and log it to the console
+// Query selector takes a valid CSS selector
+const box = document.querySelector('.box');
+box.style.backgroundColor = 'green';
 
 // TODO: add an event listener to the .box element that toggles the .left class
 
+box.addEventListener('click', () => {
+    box.classList.toggle('left');
+})
+
 // TODO: get a reference to #button, #txt and #output
 
+const button = document.querySelector('#button');
+const txt = document.querySelector('#txt');
+const output = document.querySelector('#output');
+
 // TODO: add an event listener to the #button that changes the text in #output to the lowercase value of #txt
+
+button.addEventListener('click', () => {
+    output.textContent = txt.value.toLowerCase();
+})
